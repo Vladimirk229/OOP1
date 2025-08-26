467178c3303802b851d6e43bdd873d93964e8004
Volodymyr Kazakov <90317545+Vladimirk229@users.noreply.github.com>
2025-08-25 23:09:41 -0400

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿namespace LibraryApp
+{
+    public class Book : MediaItem
+    {
+        public string Genre { get; set; }
+        public string ISBN { get; set; }
+
+        public Book(string title, string author, string genre, string isbn)
+            : base(title, author)
+        {
+            Genre = genre;
+            ISBN = isbn;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Author: {Author}");
+            Console.WriteLine($"Genre: {Genre}");
+            Console.WriteLine($"ISBN: {ISBN}");
+            Console.WriteLine(new string('-', 30));
+        }
+    }
+}
